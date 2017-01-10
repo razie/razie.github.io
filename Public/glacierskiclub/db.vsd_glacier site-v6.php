8396c96c9b8681d31cec63b436e0abf3818c35cf
Razvan Cojocaru <pub@razie.com>
2017-01-10 16:11:24 -0500

    initial

---
+++ Public/glacierskiclub/db.vsd_glacier site-v6.php
@@ -0,0 +1,40 @@
+<?php exit(); ?>
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\index.html	7895
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\events.html	7432
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\Programs.html	11666
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\contact.html	6130
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\membership.html	6117
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_0.jpg	12213
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_1.jpg	173133
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_2.jpg	23581
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_3.jpg	1132
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_4.jpg	1279
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_5.jpg	2081
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_6.jpg	1779
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_7.jpg	1815
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\Pano home page.jpg	172642
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\Trillium.gif	5945
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_10.jpg	122041
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_11.jpg	746
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_12.jpg	1132
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_13.jpg	1279
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\GlacierLOGO.jpg	9088
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\Skiesta 2010.jpg	47755
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\Skiesta 09.jpg	58863
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_20.jpg	109644
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_22.jpg	1132
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_23.jpg	1279
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\graecie K1.jpg	17545
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\mitchell k2.jpg	28267
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\iain NG.jpg	24795
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\StephanieJ.jpg	24853
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_32.jpg	260305
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_34.jpg	235992
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\TEAM2006s.jpg	117160
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\Copy of JFL_1206.JPG	129313
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_37.jpg	1132
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_38.jpg	1279
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_43.jpg	4310
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_45.jpg	1132
+C:\DOCUME~1\Owner\LOCALS~1\Temp\vsd_tmpdir\files\IMG_46.jpg	1279
+C:\Documents and Settings\Owner\My Documents\Glacier\glacier site-v6_website\files\Download\RegistrationForm2010.pdf	137826
